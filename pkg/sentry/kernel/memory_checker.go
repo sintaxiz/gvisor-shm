fbf2b3c88a32f6cc60f14b75e4f52cc81526005a
sintaxiz <natashaleind@gmail.com>
2023-04-25 07:13:28 +0300

    add error return

--- pkg/sentry/kernel/memory_checker.go
+++ pkg/sentry/kernel/memory_checker.go
@@ -6,6 +6,7 @@ import (
 	"syscall"
 	"unsafe"
 
+	"golang.org/x/sys/unix"
 	"gvisor.dev/gvisor/pkg/log"
 )
 
@@ -27,6 +28,7 @@ const (
 	InitFreeAddress  = 0x7f45221f8000
 	PidMemoryAddress = 0x7f45221f6000
 	ElfAddr          = 0x7f49221f6000
+	SelectorAddress  = 0x7f45221f9000
 
 	// mremap syscall constants
 	MREMAP_FIXED   = 0x2
@@ -40,7 +42,7 @@ const (
 )
 
 // continuously check memory
-func (smm *SharedMemoryManager) Start() {
+func (smm *SharedMemoryManager) Start() error {
 	attachToCPU()
 	log.Debugf("Start smm thread.")
 	log.Debugf("Address for reading pid = %x", smm.pidPointer)
@@ -77,21 +79,17 @@ func (smm *SharedMemoryManager) Start() {
 
 		}
 	}
+	return nil
 }
 
-func (smm *SharedMemoryManager) getPid() int {
-	return *(*int)(smm.pidPointer)
+// set flag to end cycle
+func (smm *SharedMemoryManager) Destroy() error {
+	log.Debugf("Destroying smm...")
+	smm.isDestroy = true
+	return nil
 }
 
-func (smm *SharedMemoryManager) setResult(res int) {
-	*(*int)(smm.resultPointer) = TestResult
-}
-
-func (smm *SharedMemoryManager) endCommunication() {
-	*(*int)(smm.pidPointer) = 0
-}
-
-func (smm *SharedMemoryManager) AddProcess(pid int, t *Task) {
+func (smm *SharedMemoryManager) AddProcess(pid int, t *Task) error {
 	log.Debugf("Adding process to smm...")
 
 	size := SizePerProcess
@@ -100,21 +98,22 @@ func (smm *SharedMemoryManager) AddProcess(pid int, t *Task) {
 	proc_mem, _, err := anonMmap(addr, size)
 	if err != 0 {
 		log.Warningf("Can not map memory, reason: %s", err.Error())
-		return
+		return fmt.Errorf("Can not map memory, reason: %s", err.Error())
 	}
 	log.Infof("proc_mem=%x for pid=%d", proc_mem, pid)
 	smm.tasks[666] = t
+	return nil
 }
 
-func (smm *SharedMemoryManager) CreateAddr(pid int) uintptr {
+func (smm *SharedMemoryManager) CreateAddr(pid int) (uintptr, error) {
 	log.Debugf("Creating address in smm for pid=%d...", pid)
 
 	new_addr_ptr := uintptr(InitFreeAddress + SizePerProcess*pid)
 	smm.memoryChunks[pid] = new_addr_ptr
-	return new_addr_ptr
+	return new_addr_ptr, nil
 }
 
-func (smm *SharedMemoryManager) AfterAddingProcess(pid int) {
+func (smm *SharedMemoryManager) AfterAddingProcess(pid int) error {
 	log.Debugf("Doing things after adding process to smm...")
 	// old_addr := DEFAULT_ADDR_FOR_NEW_PROC
 	// new_addr := smm.createAddr(pid)
@@ -126,17 +125,18 @@ func (smm *SharedMemoryManager) AfterAddingProcess(pid int) {
 	if err != 0 {
 		log.Debugf("Can not mremap memory for smm, reason: %s", err.Error())
 		fmt.Println(err)
-		return
+		return fmt.Errorf("Can not mremap memory for smm, reason: %s", err.Error())
 	}
 	smm.memoryChunks[pid] = new_addr_ptr
 	log.Infof("new_addr=%x for pid=%d", new_addr_ptr, pid)
-
+	return nil
 }
 
-func (smm *SharedMemoryManager) DeleteProcess(pid int) {
+func (smm *SharedMemoryManager) DeleteProcess(pid int) error {
 	log.Debugf("Deleting process from smm...")
 
 	delete(smm.memoryChunks, pid)
+	return nil
 }
 
 // create memory chunk
@@ -158,6 +158,15 @@ func (smm *SharedMemoryManager) CreateMemory(size int) error {
 	smm.pidPointer = unsafe.Pointer(shmAddr)
 	smm.resultPointer = unsafe.Pointer(shmAddr + unsafe.Sizeof(int(0)))
 
+	// for SUD
+	selectorAddr, _, err := anonMmap(SelectorAddress, size)
+	if err != 0 {
+		log.Debugf("Can not map memory for selectorAddr, reason: %s", err.Error())
+		return err
+	}
+	log.Debugf("Selector addr: %x", selectorAddr)
+	*(*int)(unsafe.Pointer(uintptr(selectorAddr))) = 1
+
 	fd, err2 := syscall.Open("/tmp/func", syscall.O_RDONLY, 0777)
 	if err2 != nil {
 		log.Debugf("Can not open file for smm, reason: %s", err2)
@@ -173,6 +182,18 @@ func (smm *SharedMemoryManager) CreateMemory(size int) error {
 	log.Debugf("Proc mem: %x", elf_mem)
 
 	return nil
+}
+
+func (smm *SharedMemoryManager) getPid() int {
+	return *(*int)(smm.pidPointer)
+}
+
+func (smm *SharedMemoryManager) setResult(res int) {
+	*(*int)(smm.resultPointer) = TestResult
+}
+
+func (smm *SharedMemoryManager) endCommunication() {
+	*(*int)(smm.pidPointer) = 0
 }
 
 func mremap(addr uint64) (new_addr_ptr, r2 uintptr, err syscall.Errno) {
@@ -206,18 +227,18 @@ func anonMmap(addr int, size int) (shmAddr, r2 uintptr, err syscall.Errno) {
 func attachToCPU() {
 	runtime.LockOSThread()
 
-	const __NR_sched_setaffinity = 203
-	var mask [1024 / 64]uint8
-	mask[1/64] |= 1 << (1 % 64)
-	_, _, errno := syscall.RawSyscall(__NR_sched_setaffinity, 0, uintptr(len(mask)*8), uintptr(unsafe.Pointer(&mask)))
-	if errno != 0 {
-		log.Debugf("Error in attachToCPU")
+	var mask unix.CPUSet
+	mask.Set(0x01)
+	err := unix.SchedSetaffinity(0, &mask)
+	if err != nil {
+		log.Debugf("Error in attachToCPU in SMM")
 	}
-}
 
-// set flag to end cycle
-func (smm *SharedMemoryManager) Destroy() {
-	log.Debugf("Destroying smm...")
-
-	smm.isDestroy = true
+	// const __NR_sched_setaffinity = 203
+	// var mask [1024 / 64]uint8
+	// mask[1/64] |= 1 << (1 % 64)
+	// _, _, errno := syscall.RawSyscall(__NR_sched_setaffinity, 0, uintptr(len(mask)*8), uintptr(unsafe.Pointer(&mask)))
+	// if errno != 0 {
+	// 	log.Debugf("Error in attachToCPU")
+	// }
 }
